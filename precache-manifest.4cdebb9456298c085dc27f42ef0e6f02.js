9464e3cb51ae910903f383e7d7c67e16fc0d6201
bohdanbershadskyi <69502515+bohdanbershadskyi@users.noreply.github.com>
2022-06-05 17:27:46 +0300

    Updates

---
+++ precache-manifest.4cdebb9456298c085dc27f42ef0e6f02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43782bd3469ffca12e45",
+    "url": "/udemy-blog/static/css/main.a04e8927.chunk.css"
+  },
+  {
+    "revision": "43782bd3469ffca12e45",
+    "url": "/udemy-blog/static/js/main.43782bd3.chunk.js"
+  },
+  {
+    "revision": "6849bea3cd2f81bdccb7",
+    "url": "/udemy-blog/static/js/1.6849bea3.chunk.js"
+  },
+  {
+    "revision": "8bec66ff5a08ae287826",
+    "url": "/udemy-blog/static/js/runtime~main.8bec66ff.js"
+  },
+  {
+    "revision": "3d99feaa0cc397d32ef3bff247849fbd",
+    "url": "/udemy-blog/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/udemy-blog/precache-manifest.4cdebb9456298c085dc27f42ef0e6f02.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/udemy-blog/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
