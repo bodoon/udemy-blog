b7c45305acb355e5373a5b0e26556dceb6d60d24
bohdanbershadskyi <69502515+bohdanbershadskyi@users.noreply.github.com>
2022-06-07 12:59:02 +0300

    api url workaround

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,4 @@
 export const apiBaseUrl =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+  window.location.href === "http://localhost:3000/"
     ? "http://localhost:8080"
     : "https://udemy-blog-rest-api.herokuapp.com";
-
-    console.log(process);
-console.log(process.env.NODE_ENV);
