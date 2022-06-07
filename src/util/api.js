50b3b3ede2a8afa3d1f4f24d6f855ac089912e4b
bohdanbershadskyi <69502515+bohdanbershadskyi@users.noreply.github.com>
2022-06-07 12:44:11 +0300

    test env

--- src/util/api.js
+++ src/util/api.js
@@ -2,3 +2,6 @@ export const apiBaseUrl =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
     : "https://udemy-blog-rest-api.herokuapp.com";
+
+    console.log(process);
+console.log(process.env.NODE_ENV);
